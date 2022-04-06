d32e70a7d6bc9ab17b99208665cea970190f33fc
luizdriven22@driven.com <luizdriven22@driven.com>
2022-04-06 09:37:15 -0300

    adicionada função de virar carta

--- scripts.js
+++ scripts.js
@@ -5,3 +5,13 @@ while (numCartas % 2 !== 0 || numCartas < 4 || numCartas > 14) {
 }
 
 document.querySelector("body").classList.remove("hidden");
+
+function turnCard (el) {
+    if (el.querySelector(".back-face-turn") !== null) {
+        el.querySelector(".back-face-turn").classList.remove("back-face-turn");
+        el.querySelector("div").classList.remove("front-face-turn");
+    } else {
+        el.querySelector("div").classList.add("front-face-turn");
+        el.querySelector(".back-face").classList.add("back-face-turn");
+    }
+}
