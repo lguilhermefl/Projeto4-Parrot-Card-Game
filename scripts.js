6e9bd4129df510fb35373f39b66963a4c924febb
luizdriven22@driven.com <luizdriven22@driven.com>
2022-04-06 11:00:38 -0300

    criao while para adicionar número de cartas informado

--- scripts.js
+++ scripts.js
@@ -1,10 +1,50 @@
-let numCartas;
+let totalCards;
+let cards = [];
+const gifs = ["images/bobrossparrot.gif","images/explodyparrot.gif","images/fiestaparrot.gif","images/metalparrot.gif","images/revertitparrot.gif","images/tripletsparrot.gif","images/unicornparrot.gif"];
+let count = 0;
 
-while (numCartas % 2 !== 0 || numCartas < 4 || numCartas > 14) {
-    numCartas = prompt("Informe o número de cartas com que deseja jogar:");
+while (totalCards % 2 !== 0 || totalCards < 4 || totalCards > 14) {
+    totalCards = prompt("Informe o número de cartas com que deseja jogar:");
 }
 
+while (cards.length < totalCards) {
+    const divGame = document.querySelector(".game");
+    cards.push(gifs[count]);
+    cards.push(gifs[count]);
+    divGame.innerHTML += 
+    `<div class="card" onclick="turnCard(this)">
+        <div class="face">
+            <img src="images/front.png" />
+        </div>
+        <div class="back-face face"></div>
+    </div>`;
+    divGame.innerHTML += 
+    `<div class="card" onclick="turnCard(this)">
+        <div class="face">
+            <img src="images/front.png" />
+        </div>
+        <div class="back-face face"></div>
+    </div>`;
+    count++;
+}
+
+function comparador() { 
+	return Math.random() - 0.5; 
+}
+
+cards.sort(comparador);
+/*
+while (backFaces.length < cards.length) {
+    let backFaces = document.querySelectorAll(".back-face");
+    const divGame = document.querySelector(".game");
+    for (let i = 0; i < totalCards; i++) {
+
+    }
+}*/
+
+
 document.querySelector("body").classList.remove("hidden");
+document.querySelector(".back-face").innerHTML = `<img src=${gifs[0]}>`;
 
 function turnCard (el) {
     if (el.querySelector(".back-face-turn") !== null) {
@@ -15,3 +55,4 @@ function turnCard (el) {
         el.querySelector(".back-face").classList.add("back-face-turn");
     }
 }
+
